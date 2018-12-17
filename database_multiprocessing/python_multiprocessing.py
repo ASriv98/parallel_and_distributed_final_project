d1eed503319ea79bfc3ec2914e0eaffb15fbed0e
Atul <atulsrivastava3@yahoo.com>
2018-12-17 16:23:46 -0500

    kind of have pack expected value working and sending text

--- database_multiprocessing/python_multiprocessing.py
+++ database_multiprocessing/python_multiprocessing.py
@@ -3,6 +3,11 @@ from multiprocessing import Process, Queue, Manager
 from twilio.rest import Client
 import time
 import random
+import MySQLdb
+import pandas as pd 
+import numpy as np 
+
+
  
 
 
@@ -14,7 +19,51 @@ def display_graphs():
  
 def pack_evaluation(q):
 
-	q.put(1)
+	
+	buy_pack = False
+
+	#current cost of packs are 2 USD
+	cost_of_pack = 200
+
+
+	#the probability of choosing specific cards 
+	#key is how it is stored in the database, value is the probability of that card showing up in a pack
+	probability_by_type = {
+	"BlackCommon"	:	0.10556,
+	"BlackUncommon"	: 	0.05163,
+	"BlackRare"		:	0.01500,
+	"BlueCommon"	:	0.10566,
+	"BlueUncommon"	:	0.04140,
+	"BlueRare"		:	0.01500,
+	"RedCommon"		:	0.03167,
+	"RedRare"		:	0.00813,
+	"GreenCommon"	:	0.10556,
+	"GreenUncommon"	:	0.04140,
+	"GreenRare"		:	0.01500,
+	"ItemCommon"	:	0.09048,
+	"ItemUncommon"	:	0.03845,
+	"ItemRare"		:	0.01500,
+	}
+
+
+	df = pd.read_csv("12-17-18.csv")
+	df2 = df.set_index("Date", drop = False)
+	top = df2.head(1)
+	#bottom = df2.values[-1].tolist()	
+	#print(bottom)
+	print(df2)
+
+	pack_expected_value = 0
+
+	for card_type in probability_by_type:
+		print(df2[card_type][-1])	
+		card_cost = df2[card_type][-1]
+		card_probability = probability_by_type[card_type]
+		pack_expected_value += card_cost * card_probability
+	
+	print(pack_expected_value)
+	if (pack_expected_value > cost_of_pack):
+		q.put(1)
 
 def price_alert(q):
 
